0996f569fc53bd5f00e5cd5a3d19f47054a34e93
Jorge Saud <jorgelsaud@gmail.com>
2016-11-29 15:52:03 -0400

    debug

--- admin/class-gs-custom-post-divi-generator-admin.php
+++ admin/class-gs-custom-post-divi-generator-admin.php
@@ -146,7 +146,8 @@ endif;
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles($hook) {
-		die($hook);
+		die(strpos($hook,'divi-custom-post'));
+
 		if(strpos($hook,'divi-custom-post')!== false){
 			/**
 			 * This function is provided for demonstration purposes only.
